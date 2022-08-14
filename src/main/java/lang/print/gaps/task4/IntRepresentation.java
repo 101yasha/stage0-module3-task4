d6106a0c9387e8f62a526d186759e47653a16d31
dzmitry <zakharenka.dzmitry.1981@gmail.com>
2022-08-14 15:14:01 +0300

    stage0-module3-task4

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -1,6 +1,23 @@
 package lang.print.gaps.task4;
 
+import java.io.PrintStream;
+
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c1 = 'c';
+        int c = 99;
+        char h1 = 'h';
+        int h = 104;
+        char a1 = 'a';
+        int a = 97;
+        char r1 = 'r';
+        int r = 114;
+    System.out.println(c);
+    System.out.println(h);
+    System.out.println(a);
+    System.out.println(r);
+
     }
 }
+
+//Print chars :'c', 'h', 'a', 'r' using int representation.
